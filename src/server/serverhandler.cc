91e359b813455df97341db75fc54236c06812456
drowzyorginal <simon.thornqvist@gmail.com>
2013-04-02 21:22:20 +0200

    added main class for server

---
+++ src/server/serverhandler.cc
@@ -0,0 +1,10 @@
+#include "server.h"
+#include "../com/messagehandler.h"
+#include "../com/connection.h"
+
+using namespace server;
+using namespace com;
+
+int main (int argc, char* argv){
+    return 0;
+}
