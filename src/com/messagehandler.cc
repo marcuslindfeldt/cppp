3e6b3a39aee5308617347c2983fbb53930565f2c
Marcus Lindfeldt <marcus.lindfeldt@gmail.com>
2013-04-04 01:43:30 +0200

    implemented messagehandler

--- src/com/messagehandler.cc
+++ src/com/messagehandler.cc
@@ -1,53 +1,98 @@
 #include "messagehandler.h"
-#include "connection.h"
+#include "protocol.h"
 
 namespace com {
 
-	void MessageHandler::sendByte(int)
+	/** 
+	* private helper method 
+	* send number ranging from 0-255
+	*/
+	void MessageHandler::sendByte(int byte) const
 	{
-
+		conn.write(static_cast<unsigned char>(byte));
 	}
 
-	void MessageHandler::sendCode(int)
+	/**
+	 * Transmit a code (a constant form the Protocol class).
+	 * @param code The code to transmit.
+	 */
+	void MessageHandler::sendCode(int code)
 	{
-
+		sendByte(code);
 	}
 
+	/**
+	 * Transmit an int value, according to the protocol
+	 * @param value The integer to transmitt
+	 */
 	void MessageHandler::sendInt(int value)
 	{
-	    conn.write((value >> 24) & 0xFF);
-	    conn.write((value >> 16) & 0xFF);
-	    conn.write((value >> 8)  & 0xFF);
-	    conn.write(value & 0xFF);
+	    sendByte((value >> 24) & 0xFF);
+	    sendByte((value >> 16) & 0xFF);
+	    sendByte((value >> 8)  & 0xFF);
+	    sendByte(value & 0xFF);
 	}
 
-	void MessageHandler::sendIntParameter(int)
+	/**
+	 * Transmit an int parameter, according to the protocol.
+	 * @param param The parameter to transmitt
+	 */
+	void MessageHandler::sendIntParameter(int param)
 	{
-
+		sendByte(Protocol::PAR_NUM);
+		sendInt(param);
 	}
 
-	void MessageHandler::sendStringParameter(std::string)
+	void MessageHandler::sendStringParameter(std::string param)
 	{
+		size_t len = param.length();
+		sendCode(Protocol::PAR_STRING);
+		sendInt(len);
 
+		for (size_t i = 0; i < len ; ++i) {
+			sendByte(param[i]);
+		}
 	}
 
-	int MessageHandler::recvCode()
+	const int MessageHandler::recvCode() const
 	{
-
+		return conn.read();
 	}
 
-	int MessageHandler::recvInt()
+	const int MessageHandler::recvInt() const
 	{
+		unsigned char b1 = conn.read();
+		unsigned char b2 = conn.read();
+		unsigned char b3 = conn.read();
+		unsigned char b4 = conn.read();
 
+		return b1 << 24 | b2 << 16 | b3 << 8 | b4;
 	}
 
-	int MessageHandler::recvIntParameter()
+	const int MessageHandler::recvIntParameter()
 	{
-
+		int code = recvCode();
+		if(code != Protocol::PAR_NUM){
+			throw ConnectionClosedException();
+		}
+		return recvInt();
 	}
 
-	std::string& MessageHandler::recvStringParameter()
+	const std::string MessageHandler::recvStringParameter()
 	{
+		int code = recvCode();
+		if(code != Protocol::PAR_STRING){
+			throw ConnectionClosedException();
+		}
+		int n = recvInt();
+		if(n < 0){
+			throw ConnectionClosedException();
+		}
+		std::string res;
 
+		// for(int i = 0; i < n; i++){
+		// 	res.push_back(conn.read());
+		// }
+		return res;
 	}
 }
--- src/com/messagehandler.h
+++ src/com/messagehandler.h
@@ -2,26 +2,26 @@
 #ifndef MESSAGEHANDLER_H
 #define MESSAGEHANDLER_H
 
-#include <string>
 #include "connection.h"
+#include <string>
 
 namespace com {
 	class MessageHandler
 	{
 	public:
-		MessageHandler(Connection c) : conn(c) {};
+		MessageHandler(Connection& c) : conn(c) {};
 		~MessageHandler();
 	
-		void sendByte(int);
 		void sendCode(int);
 		void sendInt(int);
 		void sendIntParameter(int);
 		void sendStringParameter(std::string);
-		int recvCode();
-		int recvInt();
-		int recvIntParameter();
-		std::string& recvStringParameter();
+		const int recvCode() const;
+		const int recvInt() const;
+		const int recvIntParameter();
+		const std::string recvStringParameter();
 	private:
+		void sendByte(int) const;
 		Connection conn;
 	};
 }
