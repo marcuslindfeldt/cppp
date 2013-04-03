43f8f5dd2c2e53b13cb1963ac4110fdb54a6bb29
drowzyorginal <simon.thornqvist@gmail.com>
2013-04-03 16:40:45 +0200

    partial implementation of database interface

--- src/database/database.h
+++ src/database/database.h
@@ -1,15 +1,22 @@
 #ifndef DATABASE_H
 #define DATABASE_H
+#include <string>
+#include "Article.h"
 
-/* superclass for inmemory database and file database
- * TODO
+/* Interface for inmemory database and file database
+ *
  */
  namespace database{
 
     class Database {
         public:
             virtual ~Database(){};
-
+            virtual const Article& getArticle(size_t ngId, size_t artId) const;
+            virtual int populateNewsgroup(const Article& art, size_t ngId);
+            virtual int deleteArticle(size_t ngId, size_t artId);
+            virtual int deleteNewsgroup(size_t ngId);
+            virtual int saveNewsgroup(const std::string& name);
+            virtual int populateArticle(size_t ngId, size_t artId);
 
     };
  }
