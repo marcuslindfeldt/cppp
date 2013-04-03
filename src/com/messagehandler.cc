1e55ad404fc9f4833571f87754dd010f683dcd2a
Marcus Lindfeldt <marcus.lindfeldt@gmail.com>
2013-04-03 23:46:26 +0200

    added skeleton for messagehandler

--- src/com/messagehandler.cc
+++ src/com/messagehandler.cc
@@ -1,6 +1,53 @@
 #include "messagehandler.h"
+#include "connection.h"
 
 namespace com {
 
+	void MessageHandler::sendByte(int)
+	{
 
+	}
+
+	void MessageHandler::sendCode(int)
+	{
+
+	}
+
+	void MessageHandler::sendInt(int value)
+	{
+	    conn.write((value >> 24) & 0xFF);
+	    conn.write((value >> 16) & 0xFF);
+	    conn.write((value >> 8)  & 0xFF);
+	    conn.write(value & 0xFF);
+	}
+
+	void MessageHandler::sendIntParameter(int)
+	{
+
+	}
+
+	void MessageHandler::sendStringParameter(std::string)
+	{
+
+	}
+
+	int MessageHandler::recvCode()
+	{
+
+	}
+
+	int MessageHandler::recvInt()
+	{
+
+	}
+
+	int MessageHandler::recvIntParameter()
+	{
+
+	}
+
+	std::string& MessageHandler::recvStringParameter()
+	{
+
+	}
 }
--- src/com/messagehandler.h
+++ src/com/messagehandler.h
@@ -2,11 +2,28 @@
 #ifndef MESSAGEHANDLER_H
 #define MESSAGEHANDLER_H
 
-namespace com {
-    class MessageHandler {
-        public:
-        private:
+#include <string>
+#include "connection.h"
 
-    };
-#endif
+namespace com {
+	class MessageHandler
+	{
+	public:
+		MessageHandler(Connection c) : conn(c) {};
+		~MessageHandler();
+	
+		void sendByte(int);
+		void sendCode(int);
+		void sendInt(int);
+		void sendIntParameter(int);
+		void sendStringParameter(std::string);
+		int recvCode();
+		int recvInt();
+		int recvIntParameter();
+		std::string& recvStringParameter();
+	private:
+		Connection conn;
+	};
 }
+
+#endif
