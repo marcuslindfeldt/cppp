f61d357acf02cf8eab2fc68e77a634328946abfc
hyperremix <fredrik.pettersson.89@gmail.com>
2013-04-03 15:16:32 +0200

    initial implementation of article

--- src/database/article.h
+++ src/database/article.h
@@ -5,10 +5,14 @@ namespace database {
 
     class Article {
         public:
+        	Article(int id, const string& title, const string& author, const stirng& text) : id(id), title(title) , author(author) , text(text) {}
+        	~Article() {}
+        	const string& getTitle() const { return title; }
+        	const string& getAuthor() const { return author; }
+        	const string& getText() const { return text; }
         private:
-
+        	int id;
+        	string title, author, text;
     };
-
-
 }
 #endif
