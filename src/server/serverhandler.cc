0199324cbf2a735b88928724d47accd5936c5f37
drowzyorginal <simon.thornqvist@gmail.com>
2013-04-03 15:21:11 +0200

    partial implementation of serverhandler

--- src/server/serverhandler.cc
+++ src/server/serverhandler.cc
@@ -1,3 +1,5 @@
+#include <iostream>
+#include <cstdlib>
 #include "server.h"
 #include "../com/messagehandler.h"
 #include "../com/connection.h"
@@ -5,6 +7,35 @@
 using namespace server;
 using namespace com;
 
-int main (int argc, char* argv){
-    return 0;
+using namespace std;
+
+int main (int argc, char** argv){
+
+    if(argc != 2) {
+        cerr << "No arguments provided " << endl;
+        exit(1);
+    }
+    Server server(atoi(argv[1]));
+    if(!server.isReady()) {
+        cerr << "Server init failed" << endl;
+        exit(1);
+    }
+    cout << "Server running at port: " << argv[1] << endl;
+    while(true) {
+        Connection* conn = server.waitForActivity();
+        try {
+            if(conn != 0) {
+                cout << "Connection established" << endl;
+            }
+            else {
+                server.registerConnection(new Connection);
+                cout << "New Client connected" << endl;
+            }
+        } catch (ConnectionClosedException&) {
+            server.deregisterConnection(conn);
+            delete conn;
+            cout << "Client closed connection" << endl;
+        }
+    }
+
 }
